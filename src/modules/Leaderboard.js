d9de542f3ff4244d8c0e38e64033db886d68f9e9
Anwar <anwarhussaini160@gmail.com>
2023-06-22 12:04:58 +0430

    Update leaderboard to hundle errors

--- src/modules/Leaderboard.js
+++ src/modules/Leaderboard.js
@@ -1,26 +1,35 @@
 class Leaderboard {
   constructor() {
-    this.apiUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Ac8da8pWzrDvUpOmkhoP/scores';
+    this.apiUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/mfnFOIKVcDAV9WvHfW7D/scores';
   }
 
   getDataFromAPI = async () => {
-    const response = await fetch(this.apiUrl);
-    const data = await response.json();
-    return data.result;
+    try {
+      const response = await fetch(this.apiUrl);
+      const data = await response.json();
+      return data.result;
+    } catch (error) {
+      return error;
+    }
   };
 
   sendDataToAPI = async (userName, score) => {
-    const data = {
-      method: 'POST',
-      body: JSON.stringify({
-        user: userName,
-        score,
-      }),
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-      },
-    };
-    await fetch(this.apiUrl, data);
+    try {
+      const data = {
+        method: 'POST',
+        body: JSON.stringify({
+          user: userName,
+          score,
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      };
+      await fetch(this.apiUrl, data);
+    } catch (error) {
+      return error;
+    }
+    return null;
   };
 }
 
